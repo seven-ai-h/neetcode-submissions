71902a56455b15f2d1695f9914e8486b55c5a6f4
hiro aihara <76855889+seven-ai-h@users.noreply.github.com>
2026-05-04 19:19:24 -0700

    Enhance submission-2.py with performance comments
    Added comments explaining the use of set for efficiency.

--- Data Structures & Algorithms/duplicate-integer/submission-2.py
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -7,3 +7,7 @@ class Solution:
             else:
                 return True
         return False
+
+#use set for runtime purposes
+#set.add is O(1) while arr.append is O(n)
+#Not in: set() -> O(1), arr() -> O(n^2)
